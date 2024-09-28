0e5507860b9ca40cda7a7c48507a68792f129c27
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-09-27 23:38:21 -0500

    Update jest config file

---
+++ jest.setup.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'react-native',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
