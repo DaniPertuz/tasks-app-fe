1c0bd916e304022ba0f3a898cb5328898f60fb40
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-09-27 23:21:20 -0500

    General styles and colors

---
+++ src/theme/appStyles.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const appStyles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    borderRadius: 10,
+    paddingHorizontal: 5,
+    paddingVertical: 10,
+    width: 80
+  }
+});
---
+++ src/theme/colors.ts
@@ -0,0 +1,11 @@
+export const appColors = {
+  primary: '#207CFD',
+  secondary: '#58D7FE',
+  tertiary: '#858585',
+  quaternary: '#DEDEDE',
+  danger: '#D13232',
+  warning: '#F9C749',
+  success: '#26DFB3',
+  text: '#081023',
+  white: '#FAFAFA'
+};
